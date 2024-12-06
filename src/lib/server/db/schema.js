fc59ab6a53806d93005ddb40ca4bca4e823baa2b
Brett Doyle <brett@netdreams.co.uk>
2024-12-06 16:06:23 +0000

    Changed schema

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,7 +1,12 @@
-import { mysqlTable, serial, text, int, varchar } from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, text, int, varchar, bigint } from 'drizzle-orm/mysql-core';
 
-export const user = mysqlTable('user', {
-	id: serial('id').primaryKey(),
-	name: varchar({ length: 255 }).notNull(),
-	email: varchar({ length: 255 }).notNull().unique()
+export const users = mysqlTable('users', {
+	id: bigint('id', { mode: 'bigint', unsigned: true }).notNull().autoincrement().primaryKey(),
+	name: varchar('name', { length: 255 }).notNull(),
+	email: varchar('email', { length: 255 }).notNull().unique()
+});
+
+export const posts = mysqlTable('posts', {
+	id: bigint('id', { unsigned: true }).notNull().autoincrement().primaryKey(),
+	text: varchar('text', { length: 512 }).notNull()
 });
