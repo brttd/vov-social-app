4c3b8d7a09ece17bc50eeefeb82abab5b7511f89
Brett Doyle <brett@netdreams.co.uk>
2024-12-30 11:57:50 +0000

    Added profile edit page

---
+++ src/routes/profile/+page.server.js
@@ -0,0 +1,136 @@
+import { hash, verify } from '@node-rs/argon2';
+import { fail, redirect } from '@sveltejs/kit';
+import * as auth from '$lib/server/auth';
+import * as db from '$lib/server/db';
+
+export async function load(event) {
+	if (!event.locals.user) {
+		return redirect(302, '/login');
+	}
+
+	return {
+		user: event.locals.user
+	};
+}
+
+export const actions = {
+	update: async (event) => {
+		if (!event.locals.user) {
+			return fail(401);
+		}
+
+		const formData = await event.request.formData();
+		const email = formData.get('email');
+
+		if (!validateEmail(email)) {
+			return fail(400, {
+				message: 'Invalid email'
+			});
+		}
+
+		if (formData.get('change_password')) {
+			const new_password = formData.get('new_password');
+			const confirm_password = formData.get('confirm_password');
+			const current_password = formData.get('current_password');
+
+			if (!validatePassword(new_password)) {
+				return fail(400, { message: 'Invalid password (min 6, max 255 characters)' });
+			}
+
+			if (new_password !== confirm_password) {
+				return fail(400, { message: 'Passwords do not match' });
+			}
+
+			const existingUser = await db
+				.db('users')
+				.where({
+					id: event.locals.user.id
+				})
+				.first();
+
+			if (!existingUser) {
+				return fail(400, { message: 'Error while changing password' });
+			}
+
+			const samePassword = await verify(existingUser.password, new_password, {
+				memoryCost: 19456,
+				timeCost: 2,
+				outputLen: 32,
+				parallelism: 1
+			});
+
+			if (samePassword) {
+				return fail(400, { message: 'New password cannot be the same as previous' });
+			}
+
+			const validPassword = await verify(existingUser.password, current_password, {
+				memoryCost: 19456,
+				timeCost: 2,
+				outputLen: 32,
+				parallelism: 1
+			});
+
+			if (!validPassword) {
+				return fail(400, { message: 'Incorrect password' });
+			}
+
+			const passwordHash = await hash(new_password, {
+				// recommended minimum parameters
+				memoryCost: 19456,
+				timeCost: 2,
+				outputLen: 32,
+				parallelism: 1
+			});
+
+			try {
+				await db
+					.db('users')
+					.where({
+						id: event.locals.user.id
+					})
+					.update({
+						password: passwordHash
+					});
+			} catch (e) {
+				console.error(e);
+
+				return fail(500, { message: 'An error has occurred while updating password' });
+			}
+		}
+
+		try {
+			await db
+				.db('users')
+				.where({
+					id: event.locals.user.id
+				})
+				.update({
+					email: email
+				});
+		} catch (e) {
+			console.error(e);
+
+			return fail(500, { message: 'An error has occurred' });
+		}
+
+		return redirect(302, '/');
+	}
+};
+
+function validatePassword(password) {
+	return typeof password === 'string' && password.length >= 6 && password.length <= 255;
+}
+
+function validateEmail(email) {
+	// TODO: Do something better than regex validation
+	return (
+		typeof email === 'string' &&
+		email.length > 3 &&
+		email.length <= 255 &&
+		email
+			.toLowerCase()
+			.match(
+				/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+			)
+	);
+}
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { enhance } from '$app/forms';
+
+	let changePassword = $state(false);
+
+	let { data, form } = $props();
+</script>
+
+<h1>Profile ({data.user.username})</h1>
+<form method="post" action="?/update" use:enhance>
+	<label>
+		Email
+		<input type="email" name="email" required value={data.user.email} />
+	</label>
+
+	<br />
+
+	<label>
+		<input type="checkbox" name="change_password" bind:checked={changePassword} value="yes" />
+		Change Password
+	</label>
+	<br />
+
+	{#if changePassword}
+		<label>
+			New Password
+			<input type="password" name="new_password" required />
+		</label>
+		<br />
+		<label>
+			Confirm New Password
+			<input type="password" name="confirm_password" required />
+		</label>
+		<br />
+		<label>
+			Current Password
+			<input type="password" name="current_password" required />
+		</label>
+		<br />
+	{/if}
+
+	<button>Update</button>
+</form>
+<p style="color: red">{form?.message ?? ''}</p>
