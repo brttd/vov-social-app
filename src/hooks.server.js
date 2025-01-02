ff5b0b49b533120a44d2505a5a79a234145d9084
Brett Doyle <brett@netdreams.co.uk>
2025-01-02 19:38:13 +0000

    Added temporary file maintenance

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,50 @@
 import * as auth from '$lib/server/auth.js';
 
+import fs from 'node:fs/promises';
+import { FILES_PATH } from '$env/static/private';
+
+// Any files older than 3 hours should be deleted
+const deleteOlderThan = 1000 * 60 * 60 * 3;
+
+async function filesMaintenanceSubfolder(dir) {
+	const list = await fs.readdir(`${FILES_PATH}/${dir}`);
+
+	const deleteBefore = Date.now() - deleteOlderThan;
+
+	for (let i = 0; i < list.length; i++) {
+		const stat = await fs.stat(`${FILES_PATH}/${dir}/${list[i]}`);
+
+		if (stat.isFile() && stat.mtime.getTime() < deleteBefore) {
+			await fs.unlink(`${FILES_PATH}/${dir}/${list[i]}`);
+		}
+	}
+}
+
+async function filesMaintenance() {
+	try {
+		const list = await fs.readdir(FILES_PATH, { recursive: false });
+
+		for (let i = 0; i < list.length; i++) {
+			const stat = await fs.stat(`${FILES_PATH}/${list[i]}`);
+
+			if (stat.isDirectory()) {
+				const temp = await fs.stat(`${FILES_PATH}/${list[i]}/temp`);
+
+				if (temp.isDirectory()) {
+					filesMaintenanceSubfolder(`${list[i]}/temp`);
+				}
+			}
+		}
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+// Run file maintenance every 30 minutes
+setInterval(filesMaintenance, 1000 * 60 * 30);
+// And when the app starts
+filesMaintenance();
+
 export const handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
 
