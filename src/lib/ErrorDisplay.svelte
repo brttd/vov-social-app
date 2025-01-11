fe7065f791504fff7513d1e577f952a6ff4030ef
Brett Doyle <brett@netdreams.co.uk>
2025-01-11 22:43:19 +0000

    Added ErrorDisplay component

---
+++ src/lib/ErrorDisplay.svelte
@@ -0,0 +1,23 @@
+<script>
+	let error = $state('');
+
+	let timeout = null;
+
+	export function hideError() {
+		error = '';
+	}
+
+	export function showError(message, time = 10) {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+
+		error = message;
+
+		timeout = setTimeout(hideError, 1000 * time);
+	}
+</script>
+
+{#if error}
+	<p style="color:red">{error}</p>
+{/if}
